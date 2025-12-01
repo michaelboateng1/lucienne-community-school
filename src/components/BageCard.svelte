eddfb432bb14057c7c65844080ca8acb27881fef
Michael Boateng <michaelboateng064@gmail.com>
2025-12-01 13:05:20 +0000

    Changed the chatbot icon color and fixed the BageCard transition

--- src/components/BageCard.svelte
+++ src/components/BageCard.svelte
@@ -4,20 +4,21 @@
 </script>
 
 <section
-	class="flex h-[100px] w-full items-center gap-5 rounded-xl px-3 py-3 transition-all duration-300 hover:scale-x-[1.1] hover:transform hover:drop-shadow-xl sm:w-[350px] sm:gap-5 sm:px-5"
-	style="background: {item.bg};"
 	data-aos="fade-up"
 	data-aos-delay={i + 150}
 >
-	<slot>
-		{#if item.icon}
-			<div class="rounded-full bg-white px-2 py-3">
-				<i class={item.icon}></i>
-			</div>
-		{/if}
-	</slot>
-	<div>
-		<p class="text-xl text-white sm:text-2xl">{item.title}</p>
-		<p class="text-md text-white sm:text-lg">{item.tag}</p>
+	<div class="flex h-[100px] w-full items-center gap-5 rounded-xl px-3 py-3 transition-all duration-300 hover:scale-x-[1.1] hover:transform hover:drop-shadow-xl sm:w-[350px] sm:gap-5 sm:px-5"
+	style="background: {item.bg};" >
+		<slot>
+			{#if item.icon}
+				<div class="rounded-full bg-white px-2 py-3">
+					<i class={item.icon}></i>
+				</div>
+			{/if}
+		</slot>
+		<div>
+			<p class="text-xl text-white sm:text-2xl">{item.title}</p>
+			<p class="text-md text-white sm:text-lg">{item.tag}</p>
+		</div>
 	</div>
 </section>
--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,7 +1,5 @@
 <script>
 	import { onMount } from 'svelte';
-	import { gsap } from 'gsap';
-	import { ScrollTrigger } from 'gsap/ScrollTrigger';
 </script>
 
 <section
